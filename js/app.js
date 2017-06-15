4196fdb744925479c64af70ec94775edf2aa44be
Kristian Kim <iondale0106@gmail.com>
2017-06-15 14:15:30 -0400

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,57 @@
+// Sticky Header
+$(window).scroll(function() {
+
+    if ($(window).scrollTop() > 100) {
+        $('.sticky_h').addClass('sticky');
+    } else {
+        $('.sticky_h').removeClass('sticky');
+    }
+});
+
+
+$('.sticky_h li a').click(function() {
+    if ($('.sticky_h').hasClass('open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.sticky_h').removeClass('open-nav');
+    }
+});
+
+// navigation scroll
+$('nav a, .tagline a').click(function(event) {
+    var id = $(this).attr("href");
+    var offset = 70;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+        scrollTop: target
+    }, 500);
+    event.preventDefault();
+});
+
+(function($){
+
+  var $modalOverlay = $('.md-overlay');
+  var $modalClose = $('.md-close');
+
+  $('*[data-target-modal]').click(function(){
+
+    var id = $(this).data('target-modal');
+    var $modal = $('#'+id);
+
+    if($modal.length){
+      $modal.show();
+      $modalOverlay.show();
+    }
+
+  });
+
+  $modalOverlay.click(function(){
+    $(this).hide();
+    $('.md-modal:visible').hide();
+  });
+
+  $modalClose.click(function(){
+    $modalOverlay.trigger('click');
+  });
+
+
+})(jQuery)
